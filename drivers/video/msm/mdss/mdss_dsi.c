4fb905eed6d412d74d5508fb05b2eb3534beb641
Christopher R. Palmer <crpalmer@gmail.com>
2014-08-28 13:42:09 -0400

    tabpro: mdss: Add device specific LP11 reset
    Change-Id: I339bb506ad7579b293f5a65c5c9de6f3454d1d22

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -654,6 +654,25 @@ static int mdss_dsi_ulps_config(struct mdss_dsi_ctrl_pdata *ctrl,
 	return mdss_dsi_ulps_config_sub(ctrl, enable);
 }
 
+#ifdef CONFIG_MACH_TABPRO
+static void lp11_hacks(struct mdss_panel_data *pdata, struct mdss_dsi_ctrl_pdata *ctrl_pdata)
+{
+	u32 tmp;
+
+	tmp = MIPI_INP((ctrl_pdata->ctrl_base) + 0xac);
+#ifdef CONFIG_MACH_MONDRIAN
+	MIPI_OUTP((ctrl_pdata->ctrl_base) + 0xac, 0x1F << 16);
+#else
+	tmp &= ~(1<<28);
+	MIPI_OUTP((ctrl_pdata->ctrl_base) + 0xac, tmp);
+#endif
+	wmb();
+	msleep(20);
+	mdss_dsi_panel_reset(pdata, 1);
+	MIPI_OUTP((ctrl_pdata->ctrl_base) + 0xac, tmp);
+}
+#endif
+
 int mdss_dsi_on(struct mdss_panel_data *pdata)
 {
 	int ret = 0;
@@ -715,8 +734,12 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 	 * Issue hardware reset line after enabling the DSI clocks and data
 	 * data lanes for LP11 init
 	 */
+#ifdef CONFIG_MACH_TABPRO
+	lp11_hacks(pdata, ctrl_pdata);
+#else
 	if (mipi->lp11_init)
 		mdss_dsi_panel_reset(pdata, 1);
+#endif
 
 	if (mipi->init_delay)
 		usleep(mipi->init_delay);
